417abefd541fba2a1e1eb49c2eb4b9edb802d5a0
JaeHoon Lim <jae.hoon.lim@cern.ch>
2019-12-09 23:07:11 +0900

    Update RGB channel & XY position

--- include/Cube.h
+++ include/Cube.h
@@ -16,18 +16,34 @@ int d_min = 800;
 int d_max = 1400; // NoCut:0 / MAX:6500 mm
 
 // Video Zoom (Crop Area)
-bool zoom_all = false; // X & Y & D
+bool zoom_all = true; // X & Y & D
 bool zoom_x = false;
 bool zoom_y = false;
 bool zoom_d = false;
 
 // Object Size Cut
-int obj_x = 50; // NoCut:0 / pixel
-int obj_y = 50; // NoCut:0 / pixel
-int obj_z = 150; // NoCut:0 / mm
+int obj_x = 150; // NoCut:0 / mm
+int obj_y = 150; // NoCut:0 / mm
+int obj_z = 50; // NoCut:0 / mm
+
+// Color Picture
+bool draw_picture = false;
+
+// Color Mask
+bool do_mask = false;
+bool mask_only = false; // Draw mask only // false : draw mask with red color
+int mask_r  = 20; // red
+int mask_g  = 20; // green
+int mask_b  = 20; // blue
+int mask_re = 20; // red +/- range
+int mask_ge = 20; // green +/- range
+int mask_be = 20; // blue +/- range
+int mask_blur = 10; // pixel
 
 // Draw Object Position Mean Line
 bool draw_mean_line = true;
 
 // Draw Object Position Graph
 bool draw_mean_graph = true;
+
+
